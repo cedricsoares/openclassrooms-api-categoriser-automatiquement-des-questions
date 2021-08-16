c6eda22902ad3f24a298edb44c0852c1f68e935d
Cedric Soares <cedricsoares@me.com>
2021-08-16 12:06:01 +0200

    finish db implementaion

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
 import random, string
 
 SECRET_KEY = "".join([random.choice(string.printable) for _ in range(24)])
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
