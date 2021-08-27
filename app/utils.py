789d58df9cf1781be2e77d121c0810eaa21b6ed8
cedric <cedricsoares@me.com>
2021-08-27 13:49:39 +0200

    Update app/utils.py to fix pca_model issue

--- app/utils.py
+++ app/utils.py
@@ -124,7 +124,7 @@ class SupervisedModel:
         self.supervised_model = pickle.load(open(filename_supervised_model, 'rb'))
         self.mlb_model = pickle.load(open(filename_mlb_model, 'rb'))
         self.tfidf_model = pickle.load(open(filename_tfidf_model, 'rb'))
-        self.pca_model = pickle.load(open(filename_pca_mdel, 'rb'))
+        self.pca_model = pickle.load(open(filename_pca_model, 'rb'))
         self.vocabulary = pickle.locad(open(filename_vocabulary, 'rb'))
 
     def predict_tags(self, supervised_model, mlb_model, text):
